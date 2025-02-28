5af34b0bceff97ca3ae201da298245e01961df19
AsMa Zh <asmazahani17@gmail.com>
2025-02-28 11:58:31 +0100

    header

--- frontend/src/pages/Client/UserInterface.jsx
+++ frontend/src/pages/Client/UserInterface.jsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 import Header from "@/pages/Client/Header/Header";
 
 const UserInterface = () => {
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 0) {
+      setIsHeaderFixed(true);
+    } else {
+      setIsHeaderFixed(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="text-base dark:text-zinc-100 min-h-screen flex flex-col relative">
-      <div className={`fixed top-0 left-0 w-full z-1000"`}>
+      <div className={`${isHeaderFixed ? "fixed top-0 left-0 w-full z-1000" : ""}`}>
         <Header />
       </div>
-      <div className="mt-20">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 };
