7c060bca215c96be7c62ec02b1ca92c66caa4bd7
AsMa Zh <asmazahani17@gmail.com>
2025-02-28 17:05:48 +0100

    model

--- frontend/src/components/Modals/ViewProduit.jsx
+++ frontend/src/components/Modals/ViewProduit.jsx
@@ -67,48 +67,51 @@ const ViewProduit = ({ produit, isOpen, onClose  }) => {
 
               {/* Section Infos */}
               <div className="w-1/2 flex flex-col">
-                <p className="text-gray-600 dark:text-gray-300">{produit.description}</p>
+                <div className="flex flex-col justify-center mt-4">
+                  <p className="text-3xl font-semibold">{produit.nom}</p>
 
-                {/* Prix */}
-                <div className="flex items-center space-x-2 mt-4">
-                  <span className="text-lg font-bold text-purpleLight">${produit.prix_apres_promo}</span>
-                  {produit.promotion_id && <span className="text-gray-400 line-through">${produit.prix}</span>}
-                </div>
+                  <div className="flex items-center space-x-2 mt-2">
+                    <span className="text-lg font-bold text-purpleLight">${produit.prix_apres_promo}</span>
+                    {produit.promotion_id && <span className="text-gray-400 line-through">${produit.prix}</span>}
+                  </div>
 
-                {/* Sélection des couleurs */}
-                <div className="mt-4">
-                  <h4 className="font-medium">Select Color:</h4>
-                  <div className="flex space-x-2 mt-2">
-                    {produit.couleurs.length > 0 ? (
-                      produit.couleurs.map((couleur) => (
-                        <button
-                          key={couleur.couleur_id}
-                          onClick={() => handleColorSelect(couleur)}
-                          className={`w-8 h-8 rounded-full border ${
-                            selectedColor?.couleur_id === couleur.couleur_id ? "border-purpleLight" : "border-gray-300"
-                          } ${couleur.pivot?.quantite === 0 ? "opacity-50 cursor-not-allowed" : ""}`}  
-                          style={{ backgroundColor: couleur.code_hex }}
-                          disabled={couleur.pivot?.quantite === 0} 
-                        />
-                      ))
-                    ) : (
-                      <p>No colors available</p>
+                  <div className="mt-4">
+                    <h4 className="font-medium">Produit description:</h4>
+                    <p className="text-gray-600 dark:text-gray-300">{produit.description}</p>
+                  </div>
+
+                  <div className="mt-4">
+                    <h4 className="font-medium">Select Color:</h4>
+                    <div className="flex space-x-2 mt-2">
+                      {produit.couleurs.length > 0 ? (
+                        produit.couleurs.map((couleur) => (
+                          <button
+                            key={couleur.couleur_id}
+                            onClick={() => handleColorSelect(couleur)}
+                            className={`w-8 h-8 rounded-full border ${
+                              selectedColor?.couleur_id === couleur.couleur_id ? "border-purpleLight" : "border-gray-300"
+                            } ${couleur.pivot?.quantite === 0 ? "opacity-50 cursor-not-allowed" : ""}`}  
+                            style={{ backgroundColor: couleur.code_hex }}
+                            disabled={couleur.pivot?.quantite === 0} 
+                          />
+                        ))
+                      ) : (
+                        <p>No colors available</p>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="mt-4">
+                    <h4 className="font-medium">Quantity:</h4>
+                    <input type="number" value={quantity} onChange={handleQuantityChange}
+                      min={1} max={selectedColor ? selectedColor.quantite : 1}  
+                      className="w-16 p-1 border rounded-md" disabled={!selectedColor} />
+                    {selectedColor && (
+                      <p className="text-sm text-gray-500">Max: {selectedColor.quantite}</p> 
                     )}
                   </div>
                 </div>
 
-                {/* Sélection de quantité */}
-                <div className="mt-4">
-                  <h4 className="font-medium">Quantity:</h4>
-                  <input type="number" value={quantity} onChange={handleQuantityChange}
-                    min={1} max={selectedColor ? selectedColor.quantite : 1}  
-                    className="w-16 p-1 border rounded-md" disabled={!selectedColor} />
-                  {selectedColor && (
-                    <p className="text-sm text-gray-500">Max: {selectedColor.quantite}</p> 
-                  )}
-                </div>
-
-                {/* Boutons */}
                 <div className="mt-auto flex justify-end gap-3 pt-4">
                   <button onClick={onClose} className="border border-purpleLight text-purpleLight py-2 px-6 rounded-md">
                     Close
