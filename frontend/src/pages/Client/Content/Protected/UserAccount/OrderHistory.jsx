57315957055ff05cb85d105ab9db34ae5422ad94
AsMa Zh <asmazahani17@gmail.com>
2025-03-06 12:58:12 +0100

    order history

--- frontend/src/pages/Client/Content/Protected/UserAccount/OrderHistory.jsx
+++ frontend/src/pages/Client/Content/Protected/UserAccount/OrderHistory.jsx
@@ -1,7 +1,29 @@
+import { useContext, useEffect, useState } from "react";
+import UserContext from '@/utils/UserContext';
+
 const OrderHistory = () => {
-    
+    const { user } = useContext(UserContext);
+
+    const [commandes, setCommandes] = useState(null);
+
+    useEffect(() => {
+        setCommandes(user.commandes);
+        console.log(user.commandes)
+    }, [user]); 
+
     return (
-        <div className="col-span-2 w-full py-2">
+        <div className="col-span-2 w-full py-2 space-y-5">
+            <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm">
+                <h1 className="text-lg font-semibold mb-4">Historique des commandes</h1>
+                <div className="grid grid-cols-1 gap-4 rounded-lg">
+                    {/* test */}
+                    {commandes?.length > 0 && (
+                        commandes.map((commande, index) => (
+                            <div key={index}>hello</div>
+                        ))
+                    )}
+                </div>
+            </div>
             <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm">
                 <h1 className="text-lg font-semibold mb-4">Historique des commandes</h1>
                 <div className="grid grid-cols-1 gap-4 rounded-lg">
