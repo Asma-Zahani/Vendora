84a90c67e4085451f57169f4f2d93b8cbb951865
AsMa Zh <asmazahani17@gmail.com>
2025-03-12 15:38:17 +0100

    statistics

--- frontend/src/components/Statistics/UsersStatistics.jsx
+++ frontend/src/components/Statistics/UsersStatistics.jsx
@@ -1,10 +1,35 @@
 import DoughnutChart from "@/components/Charts/DoughnutChart";
 
 const UsersStatistics = () => {
+  const dataGenre = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {/* Commandes récentes (colspan 2 pour les petites tailles d'écran) */}
       <div className="bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full sm:col-span-2">
           <div className="flex items-center justify-center h-100">
             <div className="flex flex-col max-w-lg">
@@ -15,40 +40,40 @@ const UsersStatistics = () => {
 
       {/* Statistiques genres - premier élément */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex items-center justify-center h-100">
-            <div className="flex flex-col">
-                Statistiques genres
-                <DoughnutChart />
+          <div className="flex flex-col h-100">
+          <h1 className="text-xl font-semibold mb-4">Statistiques genres</h1>
+            <div className="flex items-center justify-center">
+                <DoughnutChart data={dataGenre} />
             </div>
           </div>
       </div>
 
       {/* Statistiques genres - deuxième élément */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex items-center justify-center h-100">
-            <div className="flex flex-col">
-                Statistiques genres
-                <DoughnutChart />
+          <div className="flex flex-col h-100">
+          <h1 className="text-xl font-semibold mb-4">Statistiques age</h1>
+            <div className="flex items-center justify-center">
+                <DoughnutChart data={dataGenre} />
             </div>
           </div>
       </div>
 
       {/* Statistiques genres - troisième élément */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex items-center justify-center h-100">
-            <div className="flex flex-col">
-                Statistiques genres
-                <DoughnutChart />
+          <div className="flex flex-col h-100">
+          <h1 className="text-xl font-semibold mb-4">Statistiques emploi</h1>
+            <div className="flex items-center justify-center">
+                <DoughnutChart data={dataGenre} />
             </div>
           </div>
       </div>
 
       {/* Statistiques genres - quatrième élément */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex items-center justify-center h-100">
-            <div className="flex flex-col">
-                Statistiques genres
-                <DoughnutChart />
+          <div className="flex flex-col h-100">
+          <h1 className="text-xl font-semibold mb-4">Statistiques des mode de connexion</h1>
+            <div className="flex items-center justify-center">
+                <DoughnutChart data={dataGenre} />
             </div>
           </div>
       </div>
