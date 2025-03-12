ebf5699ec165fd549a3ccb137fb5a253d7286e6b
AsMa Zh <asmazahani17@gmail.com>
2025-03-12 17:36:10 +0100

    statistics

---
+++ frontend/src/components/Charts/PieChart.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const PieChart = ({ data }) => {
+  return <Pie data={data} />;
+};
+
+export default PieChart;
--- frontend/src/components/Statistics/UsersStatistics.jsx
+++ frontend/src/components/Statistics/UsersStatistics.jsx
@@ -1,25 +1,33 @@
 import DoughnutChart from "@/components/Charts/DoughnutChart";
+import PieChart from "@/components/Charts/PieChart";
 
 const UsersStatistics = () => {
   const dataGenre = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Homme', 'Femme'],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Répartition par genre',
+        data: [55, 45], // Exemple : 55% hommes, 45% femmes
+        backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const dataAge = {
+    labels: ['Enfant', 'Jeune', 'Adulte'],
+    datasets: [
+      {
+        label: 'Répartition par âge',
+        data: [30, 40, 20], // Exemple : 30% ont entre 18-25 ans, etc.
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
           'rgba(255, 159, 64, 0.2)',
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
           'rgba(255, 159, 64, 1)',
         ],
@@ -28,54 +36,89 @@ const UsersStatistics = () => {
     ],
   };
 
+  const dataEmploi = {
+    labels: ["Employé", "Sans emploi", "Retraité", "Indépendant"],
+    datasets: [
+      {
+        label: 'Répartition par emploi',
+        data: [25, 50, 15, 8], // Exemple : 50% salariés, etc.
+        backgroundColor: [
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 206, 86, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const dataConnexion = {
+    labels: ['Smartphone', 'Tablette', 'Pc'],
+    datasets: [
+      {
+        label: 'Modes de connexion',
+        data: [50, 35, 20], // Exemple : 60% via email, etc.
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {/* Bloc commandes récentes */}
       <div className="bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full sm:col-span-2">
-          <div className="flex items-center justify-center h-100">
-            <div className="flex flex-col max-w-lg">
-              Commandes récentes
-            </div>
-          </div>
+        <div className="flex items-center justify-center h-100">
+          <div className="flex flex-col max-w-lg">Commandes récentes</div>
+        </div>
       </div>
 
-      {/* Statistiques genres - premier élément */}
+      {/* Statistiques genres */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex flex-col h-100">
-          <h1 className="text-xl font-semibold mb-4">Statistiques genres</h1>
-            <div className="flex items-center justify-center">
-                <DoughnutChart data={dataGenre} />
-            </div>
-          </div>
+        <h1 className="text-xl font-semibold mb-4">Répartition par genre</h1>
+        <div className="flex items-center justify-center">
+          <DoughnutChart data={dataGenre} />
+        </div>
       </div>
 
-      {/* Statistiques genres - deuxième élément */}
+      {/* Statistiques âges */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex flex-col h-100">
-          <h1 className="text-xl font-semibold mb-4">Statistiques age</h1>
-            <div className="flex items-center justify-center">
-                <DoughnutChart data={dataGenre} />
-            </div>
-          </div>
+        <h1 className="text-xl font-semibold mb-4">Répartition par âge</h1>
+        <div className="flex items-center justify-center">
+          <DoughnutChart data={dataAge} />
+        </div>
       </div>
 
-      {/* Statistiques genres - troisième élément */}
+      {/* Statistiques emploi */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex flex-col h-100">
-          <h1 className="text-xl font-semibold mb-4">Statistiques emploi</h1>
-            <div className="flex items-center justify-center">
-                <DoughnutChart data={dataGenre} />
-            </div>
-          </div>
+        <h1 className="text-xl font-semibold mb-4">Répartition par emploi</h1>
+        <div className="flex items-center justify-center">
+          <DoughnutChart data={dataEmploi} />
+        </div>
       </div>
 
-      {/* Statistiques genres - quatrième élément */}
+      {/* Statistiques connexion */}
       <div className="overflow-hidden bg-customLight dark:bg-customDark border border-contentLight dark:border-borderDark rounded-lg p-6 shadow-sm w-full">
-          <div className="flex flex-col h-100">
-          <h1 className="text-xl font-semibold mb-4">Statistiques des mode de connexion</h1>
-            <div className="flex items-center justify-center">
-                <DoughnutChart data={dataGenre} />
-            </div>
-          </div>
+        <h1 className="text-xl font-semibold mb-4">Modes de connexion</h1>
+        <div className="flex items-center justify-center">
+          <PieChart data={dataConnexion} />
+        </div>
       </div>
     </div>
   );
