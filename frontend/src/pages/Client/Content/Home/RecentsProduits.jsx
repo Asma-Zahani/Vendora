573ec7b9f46d0435350738813fb01e447f4130c7
AsMa Zh <asmazahani17@gmail.com>
2025-04-11 20:54:56 +0100

    recent produits

--- frontend/src/pages/Client/Content/Home/RecentsProduits.jsx
+++ frontend/src/pages/Client/Content/Home/RecentsProduits.jsx
@@ -20,27 +20,31 @@ const RecentsProduits = () => {
 
   return (
     <div className="px-8 py-12">
-      <div className="flex flex-col items-center text-center mb-5">
-        <div className="flex items-center w-full justify-center">
-          <div className="border-t-2 w-20 mr-5"></div> 
-          <h1 className="text-2xl font-semibold uppercase">Produits récents</h1>
-          <div className="border-t-2 w-20 ml-5"></div> 
-        </div>
-        <p className="italic text-gray-500">Nouveautés cette semaine</p>
-      </div>
-      <div className="mt-10 gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-        {produits.length > 0 &&
-          produits.map((produit, index) => {
-            return <Card key={index} wishlist={wishlist} produit={produit} ajouterAuPanier={ajouterAuPanier} ajouterAuListeSouhait={ajouterAuListeSouhait} />;
-          })
-        }
-      </div>
+      {produits.length > 0 && 
+        <>
+          <div className="flex flex-col items-center text-center mb-5">
+            <div className="flex items-center w-full justify-center">
+              <div className="border-t-2 w-20 mr-5"></div> 
+              <h1 className="text-2xl font-semibold uppercase">Produits récents</h1>
+              <div className="border-t-2 w-20 ml-5"></div> 
+            </div>
+            <p className="italic text-gray-500">Nouveautés cette semaine</p>
+          </div>
+          <div className="mt-10 gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+            {
+              produits.map((produit, index) => {
+                return <Card key={index} wishlist={wishlist} produit={produit} ajouterAuPanier={ajouterAuPanier} ajouterAuListeSouhait={ajouterAuListeSouhait} />;
+              })
+            }
+          </div>
 
-      <div className="flex justify-center">
-        <Link to={"/shop"} className="mt-4 bg-purpleLight text-white text-[14px] py-2 px-6 rounded-md">
-          Voir tous les produits
-        </Link>
-      </div>
+          <div className="flex justify-center">
+            <Link to={"/shop"} className="mt-4 bg-purpleLight text-white text-[14px] py-2 px-6 rounded-md">
+              Voir tous les produits
+            </Link>
+          </div>
+        </>
+      }
     </div>
 
   );
