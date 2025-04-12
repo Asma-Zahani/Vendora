1bb48208f74ba67d77b4b2aa764f25491af959eb
AsMa Zh <asmazahani17@gmail.com>
2025-04-12 11:37:25 +0100

    recent produits

--- frontend/src/pages/Client/Content/Home/RecentsProduits.jsx
+++ frontend/src/pages/Client/Content/Home/RecentsProduits.jsx
@@ -43,7 +43,7 @@ const RecentsProduits = () => {
           </div>
 
           <div className="flex justify-center">
-            <Link to={"/shop"} className="mt-4 bg-purpleLight text-white text-[14px] py-2 px-6 rounded-md">
+            <Link to={"/shop"} className="mt-8 border-2 text-purpleLight text-lg py-2 px-6 rounded-lg">
               Voir tous les produits
             </Link>
           </div>
