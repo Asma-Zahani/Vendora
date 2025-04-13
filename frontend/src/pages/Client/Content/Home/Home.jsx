f6f662eec0471560c9defa56e7b9f2a10f8fa81f
AsMa Zh <asmazahani17@gmail.com>
2025-04-13 12:28:22 +0100

    affichage preference

--- frontend/src/pages/Client/Content/Home/Home.jsx
+++ frontend/src/pages/Client/Content/Home/Home.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
+import UserContext from '@/utils/UserContext';
 import Hero from "./Hero";
 import Features from "./Features";
 import RecentsProduits from "./RecentsProduits";
+import PreferencesModal from "@/components/Modals/PreferencesModal";
 // import ProductRecommendations from './ProductRecommendations';
 
 const Home = () => {
-
+  const {user} = useContext(UserContext);
+  const showPreferencesModal = user && !user.preferences;
+  
   return (
     <div className="duration-200">
+      {showPreferencesModal && <PreferencesModal />}
       <Hero />
       <Features />
       <RecentsProduits />
