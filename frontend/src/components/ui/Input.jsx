6457623c4034756ba23dd9f06f17f8d3f7932c0a
AsMa Zh <asmazahani17@gmail.com>
2025-04-24 23:15:26 +0100

    input

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -2,11 +2,11 @@
 
 const Input = ({type = "text", name, value, onChange, onClick, placeholder = "", width = "", error, required = false, readOnly = false}) => {
   return (
-    <div>
+    <>
       <input type={type} name={name} value={value} onChange={onChange} onClick={onClick} placeholder={placeholder} required={required} readOnly={readOnly}
         className={`mt-1 px-4 py-2 text-gray-700 dark:text-grayDark dark:bg-contentDark focus:outline-none rounded-md border border-gray-300 dark:border-borderDark placeholder:text-gray-300 dark:placeholder:text-grayDark ${width ? width : "w-full"} ${onClick ? "cursor-pointer" : ""}`}/>
       {error && <p className="text-red-500 text-sm">{error}</p>}
-    </div>
+    </>
   );
 };
 
