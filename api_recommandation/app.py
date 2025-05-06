0c4be4a66e8b201324912f5a47f9595ecadb4918
AsMa Zh <asmazahani17@gmail.com>
2025-05-06 19:37:15 +0100

    recommandation

--- api_recommandation/app.py
+++ api_recommandation/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from utils import (
     users, produits, interactions,
-    filtrer_produits_preferes, get_model , get_modelInteraction
+    filtrer_produits_preferes, get_model, load_data
 )
 
 app = Flask(__name__)
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route("/recommander-produits", methods=["POST"])
 def recommander_produits():
     data = request.get_json()
-
+    users, produits, interactions = load_data()
+    
     if not isinstance(data, dict) or "user_id" not in data:
         produits_populaires = interactions.groupby("produit_id")["achat"].sum().sort_values(ascending=False)
         top_ids = produits_populaires.head(8).index.tolist()
@@ -22,7 +23,6 @@ def recommander_produits():
     user_interactions = interactions[interactions["user_id"] == user_id]
 
     if user_interactions.empty:
-        model = get_model()
         user = users[users['user_id'] == user_id].iloc[0]
 
         produits_filtres = filtrer_produits_preferes(user, produits)
@@ -30,6 +30,9 @@ def recommander_produits():
 
         produits_filtres = produits_filtres.merge(user.to_frame().T, on='user_id', how='left')
         X = produits_filtres[['user_id', 'produit_id', 'genre', 'age', 'categorie_id', 'marque_id', 'prix']]
+        
+        model = get_model()
+
         X['label'] = model.predict_proba(X)[:, 1]
         top_reco = X.sort_values(by='label', ascending=False).head(8)
 
@@ -39,45 +42,9 @@ def recommander_produits():
         return jsonify({"message": "Cet utilisateur n'existe pas dans les interactions.", "data": produits_recommandes.to_dict(orient="records")}), 200
 
     if len(user_interactions) < 10:
-        produits_populaires = interactions.groupby("produit_id")["achat"].sum().sort_values(ascending=False)
-        top_ids = produits_populaires.head(10).index.tolist()
-        produits_recommandes = produits[produits["produit_id"].isin(top_ids)]
-        return jsonify({"message": "Pas assez d'interactions. Recommandation basée sur les produits populaires.", "data": produits_recommandes.to_dict(orient="records")}), 200
+        return jsonify({"message": "Pas assez d'interactions. Recommandation basée sur les produits populaires."}), 200
         
-   # Charger le modèle d'interaction
-    model_Interaction = get_modelInteraction()
-
-    # Trouver tous les produits que l'utilisateur a déjà vus
-    produits_vus_ids = interactions[interactions["user_id"] == user_id]["produit_id"].unique()
-
-    # Filtrer les produits que l'utilisateur n’a PAS encore vus
-    produits_a_predire = produits[~produits["produit_id"].isin(produits_vus_ids)].copy()
-
-    # Ajouter les infos utilisateur
-    user_info = users[users["user_id"] == user_id].iloc[0]  # Une seule ligne
-
-    # Répéter les infos utilisateur pour tous les produits
-    produits_a_predire["user_id"] = user_info["user_id"]
-    produits_a_predire["genre"] = user_info["genre"]
-    produits_a_predire["age"] = user_info["age"]
-
-    # Vérifier les colonnes requises
-    required_cols = ['user_id', 'produit_id', 'genre', 'age', 'categorie_id', 'marque_id', 'prix']
-    if not all(col in produits_a_predire.columns for col in required_cols):
-        return jsonify({'error': f"Colonnes manquantes pour prédiction: {required_cols}"}), 400
-
-    # Prédire les scores
-    X_pred = produits_a_predire[required_cols].copy()
-    produits_a_predire["predicted_score"] = model_Interaction.predict(X_pred)
-
-    # Trier par score
-    top_reco = produits_a_predire.sort_values("predicted_score", ascending=False).head(10)
-
-    # Retourner le résultat
-    return jsonify({
-        "message": "Recommandation basée sur les interactions de l'utilisateur.",
-        "data": top_reco.to_dict(orient="records")
-    }), 200
+    return jsonify({"message": "Recommandation basée sur les interactions de l'utilisateur."}), 200
 
 
 @app.route("/")
--- api_recommandation/utils.py
+++ api_recommandation/utils.py
@@ -5,15 +5,6 @@ import joblib
 from datetime import datetime
 from sklearn import preprocessing
 
-url = "https://vendora-app.up.railway.app/api"
-users = pd.DataFrame(requests.get(url + "/users").json())
-produits = pd.DataFrame(requests.get(url + "/produits").json())
-interactions = pd.DataFrame(requests.get(url + "/interactions").json())
-preferences = pd.DataFrame(requests.get(url + "/userPreferences").json())
-
-model = None  # Variable globale
-modelInteraction = None
-
 def calculate_age(birth_date):
     today = datetime.now()
     return today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
@@ -40,28 +31,32 @@ def safe_literal_eval(val):
     except Exception:
         return []
 
-produits["categorie_id"] = produits["sous_categorie"].apply(lambda x: x["categorie_id"] if isinstance(x, dict) and "categorie_id" in x else None)
-preferences["preferred_categorie_ids"] = preferences["preferred_categorie_ids"].apply(safe_literal_eval)
-preferences["preferred_marque_ids"] = preferences["preferred_marque_ids"].apply(safe_literal_eval)
+def load_data():
+    url = "https://vendora-app.up.railway.app/api"
+    users = pd.DataFrame(requests.get(url + "/users").json())
+    produits = pd.DataFrame(requests.get(url + "/produits").json())
+    interactions = pd.DataFrame(requests.get(url + "/interactions").json())
+    preferences = pd.DataFrame(requests.get(url + "/userPreferences").json())
 
-users = users.rename(columns={"id": "user_id"})
-users = users.merge(preferences, on='user_id', how='left')
+    preferences["preferred_categorie_ids"] = preferences["preferred_categorie_ids"].apply(safe_literal_eval)
+    preferences["preferred_marque_ids"] = preferences["preferred_marque_ids"].apply(safe_literal_eval)
 
-le = preprocessing.LabelEncoder()
-le.fit(users.genre)
-users.genre = le.transform(users.genre)
+    users = users.rename(columns={"id": "user_id"})
+    users = users.merge(preferences, on='user_id', how='left')
 
-users["date_naissance"] = pd.to_datetime(users["date_naissance"])
-users['age'] = users['date_naissance'].apply(calculate_age)
+    le = preprocessing.LabelEncoder()
+    le.fit(users.genre)
+    users.genre = le.transform(users.genre)
+
+    users["date_naissance"] = pd.to_datetime(users["date_naissance"])
+    users['age'] = users['date_naissance'].apply(calculate_age)
+
+    produits["categorie_id"] = produits["sous_categorie"].apply(lambda x: x["categorie_id"] if isinstance(x, dict) and "categorie_id" in x else None)
+
+    return users, produits, interactions
 
 def get_model():
     global model
     if model is None:
         model = joblib.load("preferences_model.pkl")
     return model
-
-def get_modelInteraction():
-    global modelInteraction
-    if modelInteraction is None:
-        modelInteraction = joblib.load("preferences_model.pkl")
-    return modelInteraction
