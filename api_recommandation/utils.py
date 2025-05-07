6597850902cb9ba64f2f2800f7f965f6989bc5fb
AsMa Zh <asmazahani17@gmail.com>
2025-05-07 07:55:49 +0100

    dev local

--- api_recommandation/utils.py
+++ api_recommandation/utils.py
@@ -5,7 +5,8 @@ import joblib
 from datetime import datetime
 from sklearn import preprocessing
 
-url = "https://vendora-app.up.railway.app/api"
+# url = "https://vendora-app.up.railway.app/api"
+url = "http://127.0.0.1:8000/api"
 
 def load_users():
     users = pd.DataFrame(requests.get(url + "/users").json())
