1b42e04eb8eef74930fdca49925dfc8b8e9c3b59
AsMa Zh <asmazahani17@gmail.com>
2025-05-12 17:08:57 +0100

    scroll

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,8 @@ import App from './App.jsx';
 import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { SuccessMessageProvider } from "@/utils/SuccessMessageContext"
+import { SuccessMessageProvider } from "@/utils/SuccessMessageContext";
+import ScrollToTop from "@/components/ScrollToTop";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
       <UserProvider>
         <ThemeProvider>
           <SuccessMessageProvider>
+            <ScrollToTop />
             <App />
           </SuccessMessageProvider>
         </ThemeProvider>
