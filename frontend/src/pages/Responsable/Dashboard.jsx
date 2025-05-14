aea6d38d6084e6be319f9dc7bc5e50a9315116f1
AsMa Zh <asmazahani17@gmail.com>
2025-05-14 19:00:17 +0100

    dashboard responsable

--- frontend/src/pages/Responsable/Dashboard.jsx
+++ frontend/src/pages/Responsable/Dashboard.jsx
@@ -1,10 +1,33 @@
-import { Home } from "lucide-react";
+import { Clock, Home, MapPin, Truck } from "lucide-react";
 import Header from "@/components/Header/DashboardSubHeader";
+import StatCard from "@/components/Statistics/StatCard";
+import { useEffect, useState } from "react";
+import { getEntities } from "@/service/EntitesService";
 
 export const Dashboard = () => {
+  const [livraisonsParEtat, setLivraisonsParEtat] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLivraisonsParEtat(await getEntities("livraisonsParEtat"));
+      } catch (error) {
+        console.error("Erreur lors de la récupération :", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header title="Tableau de bord" icon={Home} parent="Tableau de bord" />
+      <section className="mx-6 py-6 space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <StatCard title="Livraisons effectuées" value={livraisonsParEtat ? livraisonsParEtat['Livrée'] : 0} icon={<Truck />} />
+          <StatCard title="En attente" value={livraisonsParEtat ? livraisonsParEtat['En attente'] : 0} icon={<Clock />} />
+          <StatCard title="Annulées" value={livraisonsParEtat ? livraisonsParEtat['Annulée'] : 0} icon={<MapPin />} />
+        </div>
+      </section>
     </>
   );
 };
